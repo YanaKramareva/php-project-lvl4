5dca3d9d840ded44a6199249db76fc7990ec201c
YanaKramareva <iana2000@mail.ru>
2022-05-21 22:44:32 +0300

    fixed welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="p-5 mb-4 bg-light border rounded-3">
+    <div class="p-5 mb-4 bg-light border rounded-4">
         <div class="container-fluid py-5">
-            <h1 class="display-4 fw-bold">{{ __('welcome.Hello from Hexlet!') }}</h1>
-            <p class="col-md-8 fs-3">{{ __('welcome.Practical programming courses!') }}</p>
+            <h1 class="display-5 fw-bold">{{ __('welcome.Hello from Hexlet!') }}</h1>
+            <p class="col-md-8 fs-4">{{ __('welcome.Practical programming courses!') }}</p>
             <a class="btn btn-info btn-lg" href="https://ru.hexlet.io" role="button">{{ __('welcome.Learn more') }}</a>
         </div>
     </div>
